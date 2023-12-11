2ac85624e48af7a2ec30817a79441522be0cc3b8
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-11 15:25:17 -0800

    add styling

--- src/Planet.js
+++ src/Planet.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import 'tachyons';
+
 
 const Planet = ({ data }) => {
     console.log("In one", data)
     const { name, climate } = data
     return (
-        <div>
-            <h1>{name}</h1>
-            <h2>{climate}</h2>
+        <div className="bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5">
+            <h2>{name}</h2>
+            <h4>Climate: {climate}</h4>
             <p></p>
         </div>
     )
